a866c53c69fdd6b6c1e2eae00c600269a2c48881
bi119aTe5hXk <billgateshxk@gmail.com>
2023-05-25 13:50:24 +0900

    Initial Commit

---
+++ RTSP4Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RTSP4Player
+//
+//  Created by bi119aTe5hXk on 2023/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
